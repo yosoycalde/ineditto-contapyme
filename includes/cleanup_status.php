a6a780722e4a5a06024d578e28b6cb4c817e7325
yosoycalde <juancalderon2005.18@gmail.com>
2025-08-08 09:46:03 -0500

    cleanup_status.php terminado

--- includes/cleanup_status.php
+++ includes/cleanup_status.php
@@ -64,7 +64,7 @@ function realizarLimpiezaForzada()
                         $archivosEliminados++;
                     }
                 }
-            }
+            }   
         }
         return [
             'success' => true,
